5ed85af85f13c79ecf52943f5f0d7aa341477ad6
Noah Shibley <archive.of.receipts@gmail.com>
2015-05-01 22:45:54 +0900

    Added class name to output img
    useful to have a class name on the output image to manipulate the image with css

--- qrcode.js
+++ qrcode.js
@@ -362,6 +362,7 @@ var QRCode;
 			this._oContext = this._elCanvas.getContext("2d");
 			this._bIsPainted = false;
 			this._elImage = document.createElement("img");
+			this._elImage.className = "qrcode-img";
 			this._elImage.alt = "Scan me!";
 			this._elImage.style.display = "none";
 			this._el.appendChild(this._elImage);
