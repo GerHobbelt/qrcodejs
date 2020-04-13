9ff6d00e4da168a681074dfa71f1f57dbd5c81b9
viccce <295759631@qq.com>
2020-04-13 19:54:00 +0800

    匹配android 10+
    # Conflicts:
    #	qrcode.js

--- qrcode.js
+++ qrcode.js
@@ -1101,7 +1101,7 @@
     if (/android/i.test(sAgent)) {
       // android
       android = true;
-      var aMat = sAgent.toString().match(/android ([0-9]\.[0-9])/i);
+			var aMat = sAgent.toString().match(/android ([0-9]+\.?[0-9]*)/i);
 
       if (aMat && aMat[1]) {
         android = parseFloat(aMat[1]);
